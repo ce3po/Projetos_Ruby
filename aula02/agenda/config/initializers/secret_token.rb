2b1cfb02860e746c15a4e0c73b25e561e458d6e0
Fabricio Luis <fabricio@ce3po.com>
2011-10-03 11:42:13 -0300

    roda <rails new agenda>

---
+++ aula02/agenda/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Agenda::Application.config.secret_token = 'cd05d29aacea58aae79ac6cae97341a0c4c274d11fb99c6403342bfac5cb3ef20cc1d64dcebd46fbec3511ae964033015826a8210d16fd172da7ffe542827f19'
