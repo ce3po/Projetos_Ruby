cc94655308dd2c4cc8f7db87abba37fa3e0238bc
Fabricio Luis <fabricio@ce3po.com>
2011-10-05 09:51:17 -0300

    agenda padrao vazio

--- aula03/agenda/config/initializers/secret_token.rb
+++ aula03/agenda/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Agenda::Application.config.secret_token = '32346d71b31504bb5eb8590c3827ffeddcf007da0db4bda890913e19a9986880de64a2296a3e47b0f47e29fd639e71ee8bcae4de548b5910f0bb78c1ed3751e0'
+Agenda::Application.config.secret_token = '4daf8617724ee0f52a29e481c10397ae6d4f9cefe7a6dfe6620194be98a6329aef0ccef34736b940deab73edc406227b6960ff0c7570aacd98ddfd7e005726a5'
