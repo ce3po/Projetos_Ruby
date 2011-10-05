4016c33c201edf41cf652e4e7a81d580d3783e82
Fabricio Luis <fabricio@ce3po.com>
2011-10-05 09:22:49 -0300

    Commit aula03

---
+++ aula03/agenda/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Agenda::Application.config.secret_token = '32346d71b31504bb5eb8590c3827ffeddcf007da0db4bda890913e19a9986880de64a2296a3e47b0f47e29fd639e71ee8bcae4de548b5910f0bb78c1ed3751e0'
