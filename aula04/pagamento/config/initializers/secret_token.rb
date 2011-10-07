61a2dd1b4c93b17981398cc72935b2c5a3fb880b
Fabricio Luis <fabricio@ce3po.com>
2011-10-07 10:30:11 -0300

    gem new pagamento -T | inclui therubyrace, heroku

---
+++ aula04/pagamento/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pagamento::Application.config.secret_token = 'ae13f61d36375e7f3a6b8165046520fc5d6c314843fad3b24eca753ca5cc95fe83cfd089db9421a5bdd83a9912face563e8221ae1e325001941033395d752527'
