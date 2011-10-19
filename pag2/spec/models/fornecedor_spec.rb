0fb43c13ab771d3dc98653c5a8092844098c5965
Julio Protzek <julioprotzek@gmail.com>
2011-10-19 09:06:18 -0300

    Melhora tratamento de testes

--- pag2/spec/models/fornecedor_spec.rb
+++ pag2/spec/models/fornecedor_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 
 describe Fornecedor do
 	it "válido com nome" do
-		Fornecedor.new(nome: 'Apple').should be_valid
+		Fornecedor.new(nome: 'Apple').should be_invalid
 	end
 
 	it "não é valido sem nome" do
@@ -21,6 +21,18 @@ describe Fornecedor do
 	end
 
 	it "não aceita CPF e CNPJ juntos" do
-		Fornecedor.new(nome: 'Fulano', cpf: '81251172296', cnpj: '00000000000191').should be_valid
+		Fornecedor.new(nome: 'Fulano', cpf: '81251172296', cnpj: '00000000000191').should be_invalid
 	end
+
+	it "não aceita CPF e CNPJ vazios" do
+		Fornecedor.new(nome: 'Fulano').should be_invalid
+	end
+
+	it "verifica cnpj se é pj" do
+		Fornecedor.new(nome: 'ACME', pj: true, cnpj: '00000000000191').should be_valid
+		Fornecedor.new(nome: 'ACME', pj: true, cpf: '81251172296').should be_invalid
+	end
+
+	it "verifica CPF quando for Pessoa física"
+	
 end
