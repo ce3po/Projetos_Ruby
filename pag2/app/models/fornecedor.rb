21527e3694eb78d09a0d2650b54ba281782f6a03
Julio Protzek <julioprotzek@gmail.com>
2011-10-19 08:57:18 -0300

    Faz tratamento de CPF e CNPJ
    Inclui procedimento para tratar CPF, CNPJ em branco (models)
    Inclui no formulário o campo de Pessoa Jurídica.
    
    bundle --without production
    bundle exec rspec spec - roda os testes
    
    rake db:migration - criar estrutura de banco
    rake db:test:prepare - criar banco de teste

--- pag2/app/models/fornecedor.rb
+++ pag2/app/models/fornecedor.rb
@@ -5,6 +5,7 @@ require "brcpfcnpj"
 class Fornecedor < ActiveRecord::Base
 	validates :nome, presence: true
 	validate :cpf_ou_cnpj
+	validate :cpfcnpj_vazio
 	usar_como_cpf :cpf
 	usar_como_cnpj :cnpj
 
@@ -13,6 +14,13 @@ class Fornecedor < ActiveRecord::Base
 #			errors[:base] << :erro_cpfComCNPJ
 			errors.add(:cpf, :erro_cpfComCNPJ)
 			errors.add(:cnpj, :erro_cnpjComCPF)
+		end
+	end
+
+	def cpfcnpj_vazio
+		if cpf.blank? and cnpj.blank?
+			errors[:base] << :erro_cpfcnpj_nao_branco
+#			errors.add(:cpf, "CPF e CNPJ não podem ser em branco.")
 		end
 	end
 end
