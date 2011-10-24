d4cb54f6e9fb92b9eb46b238863e176d6177952a
Fabricio Luis <fabricio@ce3po.com>
2011-10-24 09:45:10 -0200

    Novo projeto para active_pagamento

---
+++ active_pagamento/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ActivePagamento::Application.config.secret_token = '29476a8cbc49d60edbc570b1625ad002f77935a6de1b1f4e37b7c91bfaa0f3f1cfd46d0b308d6b8c43462eccdc54832bb0f2128eab15d01748b90731e9b98a41'
